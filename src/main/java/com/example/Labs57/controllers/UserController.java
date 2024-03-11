ab8fbf33a6c3e84e0a98e3dc7f46713fa198f191
motherpleasedonotlook <5775gpg68@gmail.com>
2024-03-11 14:43:16 +0300

    added security

---
+++ src/main/java/com/example/Labs57/controllers/UserController.java
@@ -0,0 +1,36 @@
+package com.example.Labs57.controllers;
+
+import com.example.Labs57.models.User;
+import com.example.Labs57.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/registration")
+    public String registration() {
+        return "registration";
+    }
+
+
+    @PostMapping("/registration")
+    public String createUser(User user) {
+        userService.createUser(user);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/hello")
+    public String securityUrl() {
+        return "hello";
+    }
+}
---
+++ src/main/java/com/example/Labs57/services/UserService.java
@@ -0,0 +1,29 @@
+package com.example.Labs57.services;
+
+import com.example.Labs57.models.User;
+import com.example.Labs57.models.enums.Role;
+import com.example.Labs57.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public boolean createUser(User user){
+        String email = user.getEmail();
+        if(userRepository.findByEmail(user.getEmail())!=null) return false;
+        user.setActive(true);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.getRoles().add(Role.ROLE_USER);
+        log.info("Saving new User with email: {}",email);
+        userRepository.save(user);
+        return true;
+    }
+
+}
